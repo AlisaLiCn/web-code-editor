3c0ffcf7f91dfee6d6d7dcb5f49f7f11065c3246
AlisaLiCn <alisalicn@gmail.com>
2023-08-23 14:54:55 +0800

    feat: init files

---
+++ packages/frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      toolbar
+    </div>
+  </header>
+
+  <main>
+    editor
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  /* header { */
+    /* display: flex; */
+    /* place-items: center; */
+    /* padding-right: calc(var(--section-gap) / 2); */
+  /* } */
+}
+</style>
